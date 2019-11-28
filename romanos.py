91237b6a3d0685207354fb474d0cdc0708e48e82
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-11-29 00:03:43 +0100

    Hasta el 3999

--- romanos.py
+++ romanos.py
@@ -1,24 +1,47 @@
 valores = {'I':1, 'V': 5, 'X':10, 'L': 50, 'C':100, 'D': 500, 'M': 1000}
+valores5 = { 'V': 5,  'L': 50,  'D': 500 } 
+simbolosOrdenados = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
 def romano_a_arabigo(numRomano):
     numArabigo = 0
     numRepes = 1
     ultimoCaracter = ''
-    for letra in numRomano:
-        if letra == ultimoCaracter:
-            numRepes +=1
-        else:
-            numRepes = 1
-        
-        if numRepes > 3:
-            return 0
-
+    for letra in numRomano: 
+        #incrementamos el valor del número arábigo con el valor numérico del símbolo romano
         if letra in valores:
             numArabigo += valores[letra]
-        else:
+            if ultimoCaracter == '':
+                pass
+            elif valores[ultimoCaracter] > valores[letra]:
+                numRepes = 1
+            elif valores[ultimoCaracter] == valores[letra]:
+                numRepes += 1
+                if letra in valores5:
+                    return 0
+
+                if numRepes > 3:
+                    return 0
+
+
+            elif valores[ultimoCaracter] < valores[letra]:
+                if numRepes > 1: # No permite repeticiones en las restas
+                    return 0
+
+                if ultimoCaracter in valores5: #No permite restas de valores de 5 (5, 50, 500)
+                    return 0
+
+                distancia = simbolosOrdenados.index(letra) - simbolosOrdenados.index(ultimoCaracter) #No permite que se resten unidades de más de un orden
+                if distancia > 2:
+                    return 0
+
+                numArabigo -= valores[ultimoCaracter]*2
+                numRepes = 1
+        else:  #si el simbolo romano no es permitido devolvemos error (0)
             return 0
 
         ultimoCaracter = letra
 
     return numArabigo
+
+romano_a_arabigo('IX')
 
--- romanos_tests.py
+++ romanos_tests.py
@@ -21,10 +21,22 @@ class RomanNumberTest(unittest.TestCase):
     def test_no_mas_de_tres_repeticiones(self):
         self.assertEqual(romano_a_arabigo('LXXIII'), 73)
         self.assertEqual(romano_a_arabigo('IIII'), 0)
+        self.assertEqual(romano_a_arabigo('VVV'), 0)
 
     def test_numeros_decrecientes(self):
+        self.assertEqual(romano_a_arabigo('IX'), 9)
         self.assertEqual(romano_a_arabigo('CMXCIX'), 999)
-        self.assertEqual(romano_a_arabigo('VC'), 0)
+ 
+    def test_restas_no_admiten_repeticiones(self):
+        self.assertEqual(romano_a_arabigo('MIIX'), 0)
+
+    def test_restas_no_admiten_derivados_del_5(self):
+        self.assertEqual(romano_a_arabigo('VC'),0)
+
+    def test_restas_no_admiten_mas_de_un_orden_de_diferencia(self):
+        self.assertEqual(romano_a_arabigo('IC'), 0)
+        self.assertEqual(romano_a_arabigo('IL'), 0)    
+        self.assertEqual(romano_a_arabigo('VL'), 0)    
 
 if __name__ == '__main__':
     unittest.main()
