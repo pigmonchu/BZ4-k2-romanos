d582ed6534ccd83530e87bd901a1ed089da0b6ad
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-12-03 00:28:43 +0100

    posible gestion de parentesis de Javier y Fran por si se desvela jota

--- romanos.py
+++ romanos.py
@@ -2,6 +2,46 @@ valores = {'I':1, 'V': 5, 'X':10, 'L': 50, 'C':100, 'D': 500, 'M': 1000}
 valores5 = { 'V': 5,  'L': 50,  'D': 500 } 
 simbolosOrdenados = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
+rangos = {
+    0: {1: 'I', 5 : 'V', 'next': 'X'},
+    1: {1: 'X', 5 : 'L', 'next': 'C'},
+    2: {1: 'C', 5 : 'D', 'next': 'M'},
+    3: {1: 'M', 'next': ''}
+}
+
+def numParentesis(cadena):
+    num = 0
+    for c in cadena:
+        if c == '(':
+            num += 1
+        else:
+            break
+    return num
+
+
+def contarParentesis(numRomano):
+    res = []
+    grupoParentesis = numRomano.split(')')
+
+    ix = 0
+    while ix < len(grupoParentesis):
+        grupo = grupoParentesis[ix]
+        numP = numParentesis(grupo)
+        if numP > 0:
+            for j in range(ix, ix+numP):
+                if grupoParentesis[j] != '':
+                   return 0
+            res.append(numP, grupo[numP:])
+            ix += numP
+
+    return res
+        
+
+
+
+
+
+
 def romano_a_arabigo(numRomano):
     numArabigo = 0
     numRepes = 1
@@ -36,6 +76,8 @@ def romano_a_arabigo(numRomano):
 
                 numArabigo -= valores[ultimoCaracter]*2
                 numRepes = 1
+        elif ultimoCaracter == ')':
+            numArabigo = numArabigo * 1000
         else:  #si el simbolo romano no es permitido devolvemos error (0)
             return 0
 
@@ -43,9 +85,27 @@ def romano_a_arabigo(numRomano):
 
     return numArabigo
 
-def arabigo_a_romano(valor):
-    1.- Descomponer valor en dígitos separando millares, centenas, decenas y unidades
-    2.- Procesar uno a uno estos dígitos y transformarlos en su forma romana 
-    3.- Ir concatenando cada resultado en una cadena completa
-    4.- Devolverla
+def invertir(cad):
+    res = ''
+    for i in range(len(cad)-1, -1, -1):
+        res+= cad[i]
+    return res
 
+def arabigo_a_romano(valor):
+    cad = invertir(str(valor))
+    res = ''
+
+    for i in range(len(cad)-1, -1, -1):
+        digit = int(cad[i])
+        if digit <= 3:
+            res += digit*rangos[i][1]
+        elif digit == 4:
+            res += (rangos[i][1]+rangos[i][5])
+        elif digit == 5:
+            res += rangos[i][5]
+        elif digit < 9:
+            res += (rangos[i][5]+rangos[i][1]*(digit-5))
+        else:
+            res += rangos[i][1]+rangos[i]['next']
+
+    return res
--- romanos_tests.py
+++ romanos_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from romanos import romano_a_arabigo, arabigo_a_romano
+from romanos import romano_a_arabigo, arabigo_a_romano, contarParentesis
 
 class RomanNumberTest(unittest.TestCase):
 
@@ -42,6 +42,13 @@ class RomanNumberTest(unittest.TestCase):
         self.assertEqual(romano_a_arabigo('(IV)'), 4000)
         self.assertEqual(romano_a_arabigo('(VII)CMXXIII'), 7923)
         self.assertEqual(romano_a_arabigo('((VII))(DLIII)DCXXXVII'), 7553637)
+
+    def test_procesar_parentesis(self):
+        self.assertEqual(contarParentesis('(IV)'), (1, 'IV'))
+        self.assertEqual(contarParentesis('((VII))(XL)CCCXXII'), [(2, 'VII'), (1, 'XL'), (0, 'CCCXXII')])
+        self.assertEqual(contarParentesis('(VI)((VII))'), 0)
+        self.assertEqual(contarParentesis('(VI)((VII)'), 0)
+        self.assertEqual(contarParentesis('VI)((VII)'), 0)
 
 class ArabicNumberTest(unittest.TestCase):
     def test_unidades(self):
